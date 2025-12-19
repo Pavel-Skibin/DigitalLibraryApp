e32964e7737f7ef5303e56cf557fd74c2f785c7d
Pavel-Skibin <SkibinPS05@gmail.com>
2025-12-19 10:15:47 +0300

    feat: core application structure and base configuration

---
+++ app/src/main/java/org/nahap/library/MyApplication.kt
@@ -0,0 +1,14 @@
+package org.nahap.library
+
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super. onCreate()
+
+        AndroidThreeTen.init(this)
+    }
+}
