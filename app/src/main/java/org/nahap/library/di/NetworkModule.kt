9275dccdd4cc491c042f95d187ebbf46cd21443f
Pavel-Skibin <SkibinPS05@gmail.com>
2025-12-27 09:53:11 +0300

    chore: setup dependency injection and add basic tests

---
+++ app/src/main/java/org/nahap/library/di/NetworkModule.kt
@@ -0,0 +1,102 @@
+package org.nahap.library.di
+
+import dagger.Module
+import dagger. Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.nahap.library.catalog. api.AuthorApi
+import org.nahap.library.catalog. api.GenreApi
+import org.nahap.library.catalog.api.LibraryApi
+import org.nahap.library.catalog.repository.LibraryRepository
+import org.nahap.library.reader.api.BookApi
+import org.nahap. library.reader.api.BookmarkApi
+import org.nahap. library.reader.parser.FB2Parser
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+//    const val BASE_URL = "http://192.168.43.43:8080/"
+//
+//    const val BASE_URL = "http://192.168.17.66:8080/"
+
+     const val BASE_URL = "http://10.0.2.2:8080/"
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level. BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit. SECONDS)
+            .writeTimeout(30, TimeUnit. SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBookApi(retrofit: Retrofit): BookApi {
+        return retrofit.create(BookApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideBookmarkApi(retrofit: Retrofit): BookmarkApi {
+        return retrofit.create(BookmarkApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLibraryApi(retrofit: Retrofit): LibraryApi {
+        return retrofit.create(LibraryApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGenreApi(retrofit: Retrofit): GenreApi {
+        return retrofit.create(GenreApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthorApi(retrofit: Retrofit): AuthorApi {
+        return retrofit.create(AuthorApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFB2Parser(): FB2Parser {
+        return FB2Parser()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLibraryRepository(
+        libraryApi: LibraryApi,
+        genreApi: GenreApi,
+        authorApi: AuthorApi
+    ): LibraryRepository {
+        return LibraryRepository(libraryApi, genreApi, authorApi)
+    }
+}
